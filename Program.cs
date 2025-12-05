1a0b34f6274aba7de69e120b327b3e0fa6d98d52
MatheusDateu <mdelmondes5@gmail.com>
2025-12-05 07:27:22 -0400

    feat: added supabase instead of json file to get my projects from cloud.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,34 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorDeploy;
-// TODO: publish develop branch to origin
+using Supabase;
+using BlazorDeploy.Services;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+#region Services
+
+#region Scoped Services
+builder.Services.AddScoped<IPortfolioService, PortfolioService>();
+#endregion
+
+#endregion
+
+#region Supabase Initialization
+var supabaseUrl = builder.Configuration["Supabase:Url"];
+var supabaseKey = builder.Configuration["Supabase:Key"];
+
+var option = new SupabaseOptions
+{
+    AutoRefreshToken = true,
+    AutoConnectRealtime = false,
+};
+
+builder.Services.AddSingleton(provider => new Supabase.Client(supabaseUrl!, supabaseKey!, option));
+#endregion
 
 await builder.Build().RunAsync();
