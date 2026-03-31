e7287859e2612b2cc67aa463b04f45848fa26ca7
MatheusDateu <mdelmondes5@gmail.com>
2026-03-31 00:01:00 -0400

    feat: added about section translations for EN, ES and PT.

--- Models/AboutMatheusDelmondesModel.cs
+++ Models/AboutMatheusDelmondesModel.cs
@@ -1,60 +1,102 @@
 ﻿using Supabase.Postgrest.Attributes;
 using Supabase.Postgrest.Models;
+using System.Globalization;
 
 namespace BlazorDeploy.Models
 {
     [Table("portf0l10-0003-_about")]
     public class AboutMatheusDelmondesModel : BaseModel
     {
+        #region Columns
         [PrimaryKey("id", false)]
         public short Id { get; set; }
         [Column("about_matheus_delmondes_title")]
-        public string AboutMeTitle { get; set; }
+        public Dictionary<string, string> AboutMeTitle { get; set; }
         [Column("about_matheus_delmondes_content")]
-        public string AboutMeContent { get; set; }
+        public Dictionary<string, string> AboutMeContent { get; set; }
         [Column("about_portfolio_title")]
-        public string AboutPortfolioTitle { get; set; }
+        public Dictionary<string, string> AboutPortfolioTitle { get; set; }
         [Column("about_portfolio_content")]
-        public string AboutPortfolioContent { get; set; }
+        public Dictionary<string, string> AboutPortfolioContent { get; set; }
         [Column("developer_mindset_title")]
-        public string DeveloperMindsetTitle { get; set; }
+        public Dictionary<string, string> DeveloperMindsetTitle { get; set; }
         [Column("developer_mindset_content")]
-        public string DeveloperMindsetContent { get; set; }
+        public Dictionary<string, string> DeveloperMindsetContent { get; set; }
         [Column("techlead_mindset_title")]
-        public string TechLeadMindsetTitle { get; set; }
+        public Dictionary<string, string> TechLeadMindsetTitle { get; set; }
         [Column("techlead_mindset_content")]
-        public string TechLeadMindsetContent { get; set; }
+        public Dictionary<string, string> TechLeadMindsetContent { get; set; }
         [Column("dotnet_and_automation_title")]
-        public string DotnetAndAutomationTitle { get; set; }
+        public Dictionary<string, string> DotnetAndAutomationTitle { get; set; }
         [Column("dotnet_and_automation_content")]
-        public string DotnetAndAutomationContent { get; set; }
+        public Dictionary<string, string> DotnetAndAutomationContent { get; set; }
         [Column("python_and_automation_title")]
-        public string PythonAndAutomationTitle { get; set; }
+        public Dictionary<string, string> PythonAndAutomationTitle { get; set; }
         [Column("python_and_automation_content")]
-        public string PythonAndAutomationContent { get; set; }
+        public Dictionary<string, string> PythonAndAutomationContent { get; set; }
         [Column("angular_and_react_title")]
-        public string AngularAndReactTitle { get; set; }
+        public Dictionary<string, string> AngularAndReactTitle { get; set; }
         [Column("angular_and_react_content")]
-        public string AngularAndReactContent { get; set; }
+        public Dictionary<string, string> AngularAndReactContent { get; set; }
         [Column("java_and_spring_title")]
-        public string JavaAndSpringTitle { get; set; }
+        public Dictionary<string, string> JavaAndSpringTitle { get; set; }
         [Column("java_and_spring_content")]
-        public string JavaAndSpringContent { get; set; }
+        public Dictionary<string, string> JavaAndSpringContent { get; set; }
         [Column("invitation_title")]
-        public string InvitationTitle { get; set; }
+        public Dictionary<string, string> InvitationTitle { get; set; }
         [Column("invitation_content")]
-        public string InvitationContent { get; set; }
+        public Dictionary<string, string> InvitationContent { get; set; }
         [Column("contact_title")]
-        public string ContactTitle { get; set; }
+        public Dictionary<string, string> ContactTitle { get; set; }
         [Column("contact_content")]
-        public string ContactContent { get; set; }
+        public Dictionary<string, string> ContactContent { get; set; }
         [Column("mobile_and_multiplatform_title")]
-        public string MobileAndMultiplatformTitle { get; set; }
+        public Dictionary<string, string> MobileAndMultiplatformTitle { get; set; }
         [Column("mobile_and_multiplatform_content")]
-        public string MobileAndMultiplatformContent { get; set; }
+        public Dictionary<string, string> MobileAndMultiplatformContent { get; set; }
         [Column("created_at")]
         public DateTimeOffset CreatedAt { get; set; }
         [Column("updated_at")]
         public DateTimeOffset UpdatedAt { get; set; }
+        #endregion
+
+        #region Methods
+        public string GetTranslated(Dictionary<string, string> field)
+        {
+            var culture = CultureInfo.CurrentCulture.Name;
+
+            var lang = culture.Split('-')[0];
+
+            if (field.ContainsKey(culture)) return field[culture];
+            if (field.ContainsKey(lang)) return field[lang];
+
+            return field.Values.FirstOrDefault() ?? string.Empty;
+        }
+        #endregion
+
+        #region Display Properties
+        public string DisplayAboutMeTitle => GetTranslated(AboutMeTitle);
+        public string DisplayAboutMeContent => GetTranslated(AboutMeContent);
+        public string DisplayAboutPortfolioTitle => GetTranslated(AboutPortfolioTitle);
+        public string DisplayAboutPortfolioContent => GetTranslated(AboutPortfolioContent);
+        public string DisplayDeveloperMindsetTitle => GetTranslated(DeveloperMindsetTitle);
+        public string DisplayDeveloperMindsetContent => GetTranslated(DeveloperMindsetContent);
+        public string DisplayTechLeadMindsetTitle => GetTranslated(TechLeadMindsetTitle);
+        public string DisplayTechLeadMindsetContent => GetTranslated(TechLeadMindsetContent);
+        public string DisplayDotnetAndAutomationTitle => GetTranslated(DotnetAndAutomationTitle);
+        public string DisplayDotnetAndAutomationContent => GetTranslated(DotnetAndAutomationContent);
+        public string DisplayPythonAndAutomationTitle => GetTranslated(PythonAndAutomationTitle);
+        public string DisplayPythonAndAutomationContent => GetTranslated(PythonAndAutomationContent);
+        public string DisplayAngularAndReactTitle => GetTranslated(AngularAndReactTitle);
+        public string DisplayAngularAndReactContent => GetTranslated(AngularAndReactContent);
+        public string DisplayJavaAndSpringTitle => GetTranslated(JavaAndSpringTitle);
+        public string DisplayJavaAndSpringContent => GetTranslated(JavaAndSpringContent);
+        public string DisplayInvitationTitle => GetTranslated(InvitationTitle);
+        public string DisplayInvitationContent => GetTranslated(InvitationContent);
+        public string DisplayContactTitle => GetTranslated(ContactTitle);
+        public string DisplayContactContent => GetTranslated(ContactContent);
+        public string DisplayMobileAndMultiplatformTitle => GetTranslated(MobileAndMultiplatformTitle);
+        public string DisplayMobileAndMultiplatformContent => GetTranslated(MobileAndMultiplatformContent);
+        #endregion
     }
 }
